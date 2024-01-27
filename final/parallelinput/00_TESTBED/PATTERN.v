09d36f9a8ab349db112867d7cc13cfe8a843e28f
TING-YU WU <125854700+wuray890521@users.noreply.github.com>
2024-01-27 15:14:26 +0800

    Add files via upload

---
+++ final/parallelinput/00_TESTBED/PATTERN.v
@@ -0,0 +1,315 @@
+//==============================================
+//==============================================					
+//												
+//	File Name		:	Final.v					
+//	Module Name		:	Fianl						
+//	Release version	:	v1.0
+//  Writer : Ray
+//												
+//==============================================
+//==============================================
+`define clk_PERIOD 20.0//# the unit of the clk_PERIOD  in here is ns 
+`define End_CYCLE 10000
+module PATTERN	(
+	// Output signals
+	rst_n,
+	clk,
+	in_valid,
+	weight_valid,
+	I,
+	W,
+	//Input signals
+	out_valid,
+	OUT
+);
+
+//------------------------------
+//	I/O Pors
+//------------------------------
+    input  out_valid;
+    input [12:0] OUT;
+    
+    output reg rst_n;
+    output reg clk;
+    output reg in_valid;
+    output reg weight_valid;
+    output reg [127:0] I;
+	  output reg [127:0] W;
+//------------------------------
+//	Parameter & Integer
+//------------------------------
+real	CYCLE = `clk_PERIOD;
+
+parameter Delay = 2.5;
+parameter Pattern_num = 50;
+
+integer seed = 2177;
+integer pat;
+integer j;
+integer i;
+integer k;
+integer l;
+integer Gol1,Gol2,Gol3,Gol4,Golden;
+
+
+integer cycles;
+integer exe_lat;
+integer out_lat;
+integer tot_lat;
+
+
+//------------------------------
+//	Register
+//------------------------------
+reg [3:0] P_weight[0:31]; //Pattern_weight
+reg [31:0] data_length;
+reg [30:0]latency;
+reg [30:0]total_latency;
+reg [3:0] Input [0:31];
+
+//reg [127:0] Input[0:(Pattern_num-1)];
+
+//------------------------------
+//	Clock
+//------------------------------
+initial clk = 1'b0;
+always #(CYCLE/2.0) clk = ~clk;
+
+/*initial clk = 1'b0;
+always #(CYCLE/2.0) clk = ~clk;
+always @(posedge clk) 
+begin
+    if (!rst_n) 
+    begin
+    latency <= 0;    
+    end 
+    else if (in_valid==1)
+    latency <= 0;
+    else begin
+    latency <= latency+1; 
+    end
+end*/
+
+//------------------------------
+//	Initial
+//------------------------------
+initial exe_task;
+/*for (pat=0 ; pat<Pattern_num ; pat=pat+1) begin
+ end*/
+task exe_task; begin 
+  
+    reset_signal_task;
+    
+    for ( pat=0 ;pat<Pattern_num ;pat=pat+1 ) 
+    begin
+    readweight_task;
+    readinput_task;
+    #(CYCLE/2);
+    input_task;
+    waitOut_task;
+    check_task;
+    $display("\033[0;34mPASS PATTERN NO.",pat+1,"\033[m \033[0;32m Cycles:%3d\033[m",latency);
+    end
+    
+    pass_task;
+end endtask
+
+task reset_signal_task; begin 
+  force clk = 0;
+    rst_n = 'b1;
+
+    in_valid = 'b0;
+    weight_valid = 'b0;
+    data_length = 'b0;
+    I = 'dx;
+    W = 'dx;
+    tot_lat = 'b0;
+    
+    #(CYCLE/2.0) rst_n = 0;
+    #(CYCLE/2.0) rst_n = 1;
+    if( OUT!==0 || out_valid!==0) begin //out!==0
+            $display("*************************************************************************");  
+            $display("*                          SPEC MAIN-1 FAIL                             *");    
+            $display("* All output signals should be reset after the reset signal is asserted *");
+            $display("*************************************************************************");
+            repeat(2) #CYCLE; //??
+            $finish;
+    end
+	#CYCLE; release clk;
+end 
+endtask
+/*
+task reset_task; begin
+	rst_n = 1;
+	in_valid = 0;
+	weight_valid = 0;
+    data_length = 0;
+	I = 'dx;
+  W = 'dx;
+    
+	tot_lat = 0;
+	#(CYCLE/2) rst_n = 0;
+    #(CYCLE/2) rst_n = 1;
+	
+	if (OUT!==0 || out_valid!==0) begin
+        $display("                                                                                                     ");
+        $display("                                                                                                     ");
+        $display("                                _ .___.-''.-=-.                                                      ");
+        $display("                    __.-- _ _.-'_)_  (''`'-._\\ `.                                                   ");
+        $display("                 _.'  __ |.' .-' __)  .-     /. |                                                    ");
+        $display("               .'_.--'   |     -'  _)/         `\\                                                   ");
+        $display("             .'.'      .'       .-'_.`           :                                                   ");
+        $display("             .'     .-'       .(.-(              :                                                   ");
+        $display("                  .'       .-' .    _            ;                                                   ");
+        $display("                 /        .-._/`---'            /                                                    ");
+        $display("                /        (____)(----. )       .'                                                     ");
+        $display("               /         (      `.__.     / \\ /.           Output signals should be 0               ");
+        $display("              :           ;-.___         /__ \\/|                                                    ");
+        $display("              |         .'      `--.      -. \\ |           after the reset singal is asserted ......");
+        $display("              :        /            \\    .__/                                                       ");
+        $display("               \\      (__            \\    |_               at %4d ps                               ", $time*1000);
+        $display("                \\       .`-. *       /   _|.\                                                       ");
+        $display("                 \\    .'   `-.     .'_.-'   \\                                                      ");
+        $display("                (_\\.-'    .'\')--.'-'       __\\                                                    ");
+        $display("                 \\       /  // .'|      .--'  `-.                                                   ");
+        $display("                  `-.    `-/ \\'  |   _.'         `.                                                 ");
+        $display("                     `-._ /      `--'/             \\                                                ");
+        $display("                        .'           |              \\                                               ");
+        $display("                       /             |               \\                                              ");
+        $display("                    .-'              |               /                                               ");
+        $display("                   /                 |             -'                                                ");
+        $display("                                                                                                     ");
+        $display("                                                                                                     ");
+        //$fclose(file);
+        repeat(5) #(CYCLE);
+        $finish;
+    end
+    #(CYCLE/2.0) release clk;
+end endtask*/
+
+integer input_file1;
+
+task readweight_task; begin
+
+    input_file1 = $fopen("../00_TESTBED/Final_projeect_PATTERN.txt","r");
+ 
+    if (input_file1==0) 
+      begin
+   	    $display("Error: Unable to open file");
+        $finish;
+      end
+
+    $fscanf (input_file1, "W1=%d,W2=%d,W3=%d,W4=%d,W5=%d,W6=%d,W7=%d,W8=%d,W9=%d,W10=%d,W11=%d,W12=%d,W13=%d,W14=%d,W15=%d,W16=%d,W17=%d,W18=%d,W19=%d,W20=%d,W21=%d,W22=%d,W23=%d,W24=%d,W25=%d,W26=%d,W27=%d,W28=%d,W29=%d,W30=%d,W31=%d,W32=%d",
+    P_weight[0],P_weight[1],P_weight[2],P_weight[3],P_weight[4],P_weight[5],P_weight[6],P_weight[7],P_weight[8],P_weight[9],P_weight[10],P_weight[11],P_weight[12],P_weight[13],P_weight[14],P_weight[15],P_weight[16],P_weight[17],P_weight[18],P_weight[19],
+    P_weight[20],P_weight[21],P_weight[22],P_weight[23],P_weight[24],P_weight[25],P_weight[26],P_weight[27],P_weight[28],P_weight[29],P_weight[30],P_weight[31]);
+  end endtask  
+
+reg [7:0] line_count = 0; //initialize reg to be 0
+reg [31:0] input_file2;
+
+task readinput_task;
+    begin
+      if (line_count == 0) begin
+        input_file2 = $fopen("../00_TESTBED/INPUTaftTrans.txt", "r");
+      end
+
+      if (input_file2 != 0) begin
+        // Read a line from the file
+        $fscanf(input_file2, "%h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h",
+                 Input[0], Input[1], Input[2], Input[3], Input[4], Input[5], Input[6], Input[7],
+                 Input[8], Input[9], Input[10], Input[11], Input[12], Input[13], Input[14], Input[15],
+                 Input[16], Input[17], Input[18], Input[19], Input[20], Input[21], Input[22], Input[23],
+                 Input[24], Input[25], Input[26], Input[27], Input[28], Input[29], Input[30], Input[31]);
+
+        // Increment line count
+        line_count = line_count + 1;
+
+        // Check if end of file is reached
+        if ($feof(input_file2)) begin
+          // Close the file and reset line count for the next iteration
+          $fclose(input_file2);
+          line_count = 0;
+        end
+      end
+    end
+  endtask
+
+
+
+task input_task; 
+begin
+    in_valid = 1;  
+    weight_valid = 1;
+    I = {Input[0], Input[1], Input[2], Input[3], Input[4], Input[5], Input[6], Input[7], Input[8], Input[9], Input[10], Input[11], Input[12], Input[13], Input[14], Input[15], 
+         Input[16], Input[17], Input[18], Input[19], Input[20], Input[21], Input[22], Input[23], Input[24], Input[25], Input[26], Input[27], Input[28], Input[29], Input[30], Input[31] };
+  
+    W = {P_weight[0], P_weight[1], P_weight[2], P_weight[3], P_weight[4], P_weight[5], P_weight[6], P_weight[7], P_weight[8], P_weight[9], P_weight[10], P_weight[11], P_weight[12],
+         P_weight[13], P_weight[14], P_weight[15], P_weight[16], P_weight[17], P_weight[18], P_weight[19], P_weight[20], P_weight[21], P_weight[22], P_weight[23], 
+         P_weight[24], P_weight[25], P_weight[26], P_weight[27], P_weight[28], P_weight[29], P_weight[30], P_weight[31]};
+   
+
+    Gol1 = Input[0]*P_weight[0]+Input[1]*P_weight[1]+Input[2]*P_weight[2]+Input[3]*P_weight[3]+Input[4]*P_weight[4]+Input[5]*P_weight[5]+Input[6]*P_weight[6]+Input[7]*P_weight[7];
+    Gol2 = Input[8]*P_weight[8]+Input[9]*P_weight[9]+Input[10]*P_weight[10]+Input[11]*P_weight[11]+Input[12]*P_weight[12]+Input[13]*P_weight[13]+Input[14]*P_weight[14]+Input[15]*P_weight[15];
+    Gol3 = Input[16]*P_weight[16]+Input[17]*P_weight[17]+Input[18]*P_weight[18]+Input[19]*P_weight[19]+Input[20]*P_weight[20]+Input[21]*P_weight[21]+Input[22]*P_weight[22]+Input[23]*P_weight[23];
+    Gol4 = Input[25]*P_weight[25]+Input[26]*P_weight[26]+Input[27]*P_weight[27]+Input[28]*P_weight[28]+Input[29]*P_weight[29]+Input[30]*P_weight[30]+Input[31]*P_weight[31]+Input[24]*P_weight[24];
+
+    Golden = Gol1 + Gol2 + Gol3 + Gol4;
+    
+    #(CYCLE);
+    in_valid = 0;    
+    I = 'dx;
+    weight_valid = 0;
+    W = 'dx;
+    
+   
+end endtask
+
+task waitOut_task; 
+begin
+ cycles = 0;
+	while(out_valid === 0)begin
+		cycles = cycles + 1;
+		if ( cycles == 100 ) begin
+			$display ("Total latency is over 10000");
+			repeat(2)@(negedge clk);
+			$finish ;			
+		end
+		@(negedge clk);
+	end
+end endtask
+
+
+task check_task; 
+begin 
+out_lat = 0;
+while (out_valid === 1)
+ begin
+    if (OUT !== Golden) 
+    begin
+    $display ("----------------------------------------------------------------------------------------------------------------------");      
+    $display ("                                                  Wrong answer!"                                       );
+	  //$display("Pattern No. 	= %d",patcount);
+	  $display ("                                               Golden_OUT 	= %d",Golden                                );
+	  $display ("                                               Your___OUT  = %d",OUT                                   );		
+	  $display ("----------------------------------------------------------------------------------------------------------------------");
+    @(negedge clk);
+	  $finish ;				    
+    end
+    @(negedge clk);
+ end
+end endtask
+
+
+task pass_task; begin
+$display ("----------------------------------------------------------------------------------------------------------------------");
+$display ("                                                  Congratulations!                						            ");
+$display ("                                           You have passed all patterns!          						            ");
+$display ("----------------------------------------------------------------------------------------------------------------------");
+repeat(5) @(negedge clk);
+$finish;	
+end endtask
+
+
+
+
+endmodule
---
+++ final/parallelinput/01_RTL/Final.v
@@ -0,0 +1,349 @@
+module Final(
+	//input
+    rst_n,
+    clk,
+    in_valid,
+    weight_valid,
+    I,
+    W,
+	//output 
+	out_valid,
+	OUT
+);
+    input rst_n;
+    input clk;
+    input in_valid;
+    input weight_valid;
+    input [127:0] I;
+    input [127:0] W;
+    output reg  out_valid;
+    output reg [12:0] OUT;
+    wire clk_gate_invalid;
+
+
+
+  reg [9:0] cnt;
+  reg [3:0] inputs [0:31];
+  reg [3:0] weight [0:31];
+
+   reg [7:0] mul_ans [0:3];
+   reg [7:0] mul_ans1 [0:3];
+   reg [7:0] mul_ans2 [0:3];
+   reg [7:0] mul_ans3 [0:3];
+   reg [7:0] mul_ans4 [0:3];
+   reg [7:0] mul_ans5 [0:3];
+   reg [7:0] mul_ans6 [0:3];
+   reg [7:0] mul_ans7 [0:3];
+
+
+   reg [8:0] add_ans1 [0:3];
+   reg [8:0] add_ans2 [0:3];
+   reg [8:0] add_ans3 [0:3];
+   reg [8:0] add_ans4 [0:3];
+   reg [9:0] add_ans5 [0:7];
+   reg [10:0] add_ans6 [0:3];
+   reg [11:0] add_ans7 [0:1];
+   wire enable;
+
+   reg flag;
+
+always @(posedge clk or negedge rst_n) 
+begin
+  if(~rst_n)
+    flag <= 0;
+  else if(in_valid)
+   flag <= 1;
+  else if(out_valid)
+   flag <= 0;
+  else 
+   flag <= flag; 
+end
+//--------counter------------------------------------------
+always @(posedge clk or negedge rst_n) //cnt
+  begin
+    if(~rst_n)
+      cnt <= 0;
+    else if(in_valid)
+      cnt <= 1;  
+    else if (flag)
+     cnt  <= cnt + 1;   
+    else if(out_valid)
+     cnt <= 0; 
+    else 
+     cnt <= cnt;
+  end
+
+  /*-----------------------------------------------------------------------------------
+          IIIIIIII  NN      NN  PPPPPPPP  UU      UU TTTTTTTTTT
+             II     NNNN    NN  PP     PP UU      UU     TT
+             II     NN  NN  NN  PP     PP UU      UU     TT
+             II     NN    NNNN  PPPPPPPP   UU    UU      TT
+          IIIIIIII  NN      NN  PP          UUUUUU       TT
+  *///---------------------------------------------------------------------------------
+  integer i,j;
+
+  always @(posedge clk or negedge rst_n) // input: I1~I32
+  begin
+    if (~rst_n)
+    begin
+     for ( i=0 ;i<32 ;i=i+1 ) 
+      begin
+        inputs [i] <= 0;  
+      end
+    end 
+   else if (in_valid)
+    begin
+      inputs[0] <= I[127:124] ;  
+      inputs[1] <= I[123:120] ;  
+      inputs[2] <= I[119:116] ;  
+      inputs[3] <= I[115:112] ;  
+      inputs[4] <= I[111:108] ;  
+      inputs[5] <= I[107:104] ;  
+      inputs[6] <= I[103:100] ;  
+      inputs[7] <= I[99:96] ;  
+      inputs[8] <= I[95:92] ;  
+      inputs[9] <= I[91:88] ;  
+      inputs[10] <= I[87:84];  
+      inputs[11] <= I[83:80];  
+      inputs[12] <= I[79:76];  
+      inputs[13] <= I[75:72];  
+      inputs[14] <= I[71:68];  
+      inputs[15] <= I[67:64];  
+      inputs[16] <= I[63:60];  
+      inputs[17] <= I[59:56];  
+      inputs[18] <= I[55:52];  
+      inputs[19] <= I[51:48];  
+      inputs[20] <= I[47:44];  
+      inputs[21] <= I[43:40];  
+      inputs[22] <= I[39:36];  
+      inputs[23] <= I[35:32];  
+      inputs[24] <= I[31:28];  
+      inputs[25] <= I[27:24];  
+      inputs[26] <= I[23:20];  
+      inputs[27] <= I[19:16];  
+      inputs[28] <= I[15:12];  
+      inputs[29] <= I[11:8];  
+      inputs[30] <= I[7:4];  
+      inputs[31] <= I[3:0];  
+    end 
+   else if (out_valid)
+    begin
+      for ( i=0 ;i<32 ;i=i+1 ) 
+      begin
+        inputs[i] <= 0;  
+      end
+    end 
+   else 
+   begin
+    for ( i=0 ;i<32 ;i=i+1 ) 
+    begin
+      inputs[i] <= inputs[i] ; 
+    end 
+  end
+   end
+
+  
+  always @(posedge clk or negedge rst_n) // input: W1~W32
+  begin
+    if (~rst_n)
+    begin
+     for ( j=0 ;j<32 ;j=j+1 ) 
+      begin
+        weight[j] <= 0;  
+      end
+    end 
+   else if(weight_valid)
+    begin
+      weight[0] <= W[127:124] ;  
+      weight[1] <= W[123:120] ;  
+      weight[2] <= W[119:116] ;  
+      weight[3] <= W[115:112] ;  
+      weight[4] <= W[111:108] ;  
+      weight[5] <= W[107:104] ;  
+      weight[6] <= W[103:100] ;  
+      weight[7] <= W[99:96] ;  
+      weight[8] <= W[95:92] ;  
+      weight[9] <= W[91:88] ;  
+      weight[10] <= W[87:84];  
+      weight[11] <= W[83:80];  
+      weight[12] <= W[79:76];  
+      weight[13] <= W[75:72];  
+      weight[14] <= W[71:68];  
+      weight[15] <= W[67:64];  
+      weight[16] <= W[63:60];  
+      weight[17] <= W[59:56];  
+      weight[18] <= W[55:52];  
+      weight[19] <= W[51:48];  
+      weight[20] <= W[47:44];  
+      weight[21] <= W[43:40];  
+      weight[22] <= W[39:36];  
+      weight[23] <= W[35:32];  
+      weight[24] <= W[31:28];  
+      weight[25] <= W[27:24];  
+      weight[26] <= W[23:20];  
+      weight[27] <= W[19:16];  
+      weight[28] <= W[15:12];  
+      weight[29] <= W[11:8];  
+      weight[30] <= W[7:4];  
+      weight[31] <= W[3:0];  
+    end 
+  end
+
+
+ /*----------------------------------------------------------------------------------------
+      CCCCCC        AAA      LL         CCCCCC  UU      UU  
+    CC             AA AA     LL       CC        UU      UU  
+   CC             AA   AA    LL      CC         UU      UU  
+    CC       C   AAAAAAAAA   LL       CC         UU    UU   
+     CCCCCCCC   AA       AA  LLLLLLLLL CCCCCCCC   UUUUUU    
+*///--------------------------------------------------------------------------------------
+ always @(posedge clk or negedge rst_n) 
+ begin
+ if (~rst_n) 
+  begin
+  for ( i=0 ;i<3 ; i=i+1) 
+   begin
+      add_ans1[i]<=0;
+      add_ans2[i]<=0;
+      add_ans3[i]<=0;
+      add_ans4[i]<=0;
+   end
+  end 
+ else if (cnt==1) begin
+  add_ans1[0] <= (inputs[0] *weight[0]) + (inputs[4] *weight[4]) ;
+  add_ans1[1] <= (inputs[1] *weight[1]) + (inputs[5] *weight[5]) ;
+  add_ans1[2] <= (inputs[2] *weight[2]) + (inputs[6] *weight[6]) ;
+  add_ans1[3] <= (inputs[3] *weight[3]) + (inputs[7] *weight[7]) ;
+
+  add_ans2[0] <= (inputs[8] *weight[8] ) + (inputs[12]*weight[12]) ;
+  add_ans2[1] <= (inputs[9] *weight[9] ) + (inputs[13]*weight[13]) ;
+  add_ans2[2] <= (inputs[10]*weight[10]) + (inputs[14]*weight[14]) ;
+  add_ans2[3] <= (inputs[11]*weight[11]) + (inputs[15]*weight[15]) ;
+
+  add_ans3[0] <= (inputs[16]*weight[16]) + (inputs[20]*weight[20]) ;
+  add_ans3[1] <= (inputs[17]*weight[17]) + (inputs[21]*weight[21]) ;
+  add_ans3[2] <= (inputs[18]*weight[18]) + (inputs[22]*weight[22]) ;
+  add_ans3[3] <= (inputs[19]*weight[19]) + (inputs[23]*weight[23]) ;
+
+  add_ans4[0] <= (inputs[24]*weight[24]) + (inputs[28]*weight[28]) ;
+  add_ans4[1] <= (inputs[25]*weight[25]) + (inputs[29]*weight[29]) ;
+  add_ans4[2] <= (inputs[26]*weight[26]) + (inputs[30]*weight[30]) ;
+  add_ans4[3] <= (inputs[27]*weight[27]) + (inputs[31]*weight[31]) ;
+ 
+  end
+  else 
+  begin
+    for ( i=0 ;i<3 ;i=i+1 ) 
+    begin
+      add_ans1[i] <= add_ans1[i];
+      add_ans2[i] <= add_ans2[i];
+      add_ans3[i] <= add_ans3[i];
+      add_ans4[i] <= add_ans4[i];
+    end
+  end
+end
+
+always @(posedge clk or negedge rst_n) // add_ans1
+ begin
+  if (~rst_n) 
+    begin
+    for ( i=0 ;i<7 ; i=i+1) 
+     begin
+      add_ans5[i] <= 0;
+     end
+    end 
+    else if (cnt==2) begin
+      add_ans5[0] <= add_ans1[0] + add_ans2[0] ;
+      add_ans5[1] <= add_ans1[1] + add_ans2[1] ;
+      add_ans5[2] <= add_ans1[2] + add_ans2[2] ;
+      add_ans5[3] <= add_ans1[3] + add_ans2[3] ;
+      add_ans5[4] <= add_ans3[0] + add_ans4[0] ;
+      add_ans5[5] <= add_ans3[1] + add_ans4[1] ;
+      add_ans5[6] <= add_ans3[2] + add_ans4[2] ;
+      add_ans5[7] <= add_ans3[3] + add_ans4[3] ;
+    end
+  else 
+  begin
+   for ( i=0 ;i<7 ;i=i+1 ) 
+   begin
+      add_ans5[i] <= add_ans5[i];
+   end 
+  end
+ end
+
+
+always @(posedge clk or negedge rst_n) // add_ans2
+ begin
+  if (~rst_n) 
+    begin
+    for ( i=0 ;i<3 ; i=i+1) 
+     begin
+      add_ans6[i]<=0;
+     end
+    end 
+   else if (cnt==3) begin
+      add_ans6[0] <= add_ans5[0] + add_ans5[2] ;
+      add_ans6[1] <= add_ans5[1] + add_ans5[3] ;
+      add_ans6[2] <= add_ans5[4] + add_ans5[6] ;
+      add_ans6[3] <= add_ans5[5] + add_ans5[7] ;
+    end
+ else 
+  begin
+   for ( i=0 ;i<3 ;i=i+1 ) 
+   begin
+      add_ans6[i]  <= add_ans6[i] ;
+   end
+  end
+ end
+
+
+always @(posedge clk or negedge rst_n) // add_ans3
+ begin
+  if (~rst_n) 
+    begin
+    for ( i=0 ;i<2 ; i=i+1) 
+     begin
+      add_ans7[i]<=0;
+     end
+    end 
+   else if (cnt==4) begin
+      add_ans7[0] <= add_ans6[0] + add_ans6[1] ;
+      add_ans7[1] <= add_ans6[2] + add_ans6[3] ;
+    end
+ else 
+  begin
+   for ( i=0 ;i<2 ;i=i+1 ) 
+   begin
+      add_ans7[i]  <= 0 ;
+   end
+  end
+ end
+
+  /*----------------------------------------------------------------------------------------------------------
+            oooooo    UU      UU TTTTTTTTTT 
+          oo      oo  UU      UU     TT     
+         oo        oo UU      UU     TT     
+          oo      oo   UU    UU      TT     
+            oooooo      UUUUUU       TT     
+  *///-------------------------------------------------------------------------------------------------------
+
+  always @(posedge clk or negedge rst_n)
+  begin
+    if (~rst_n)
+      out_valid <= 0;
+    else if (cnt==5) 
+      out_valid <=1;
+    else
+         out_valid <= 0;
+  end
+always @(posedge clk or negedge rst_n)
+  begin
+    if (~rst_n)
+      OUT <= 0;
+    else if(cnt==5)
+       OUT <= add_ans7[0] + add_ans7[1]; 
+    else 
+        OUT <= 0;
+  end
+        
+endmodule
