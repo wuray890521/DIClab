9d7ce6103c209ac63e707ec843273c5f5e682579
TING-YU WU <125854700+wuray890521@users.noreply.github.com>
2024-02-01 23:54:06 +0800

    Add files via upload

---
+++ lab2/2-3-4bitadder/Example_for_Verilog/03_GATE/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1698483401 /RAID2/COURSE/dic/dic324/ex2/Ex2_Supplement/Example_for_Verilog/03_GATE/asap7sc7p5t_SIMPLE_RVT_TT_08302018.v
+1698199521 /RAID2/COURSE/dic/dic324/ex2/Ex2_Supplement/Example_for_Verilog/01_RTL/TESTBED_Adder_4bit.v
+1698483447 /RAID2/COURSE/dic/dic324/ex2/Ex2_Supplement/Example_for_Verilog/03_GATE/asap7sc7p5t_SEQ_RVT_TT_08302018.v
+1698483491 /RAID2/COURSE/dic/dic324/ex2/Ex2_Supplement/Example_for_Verilog/03_GATE/asap7sc7p5t_INVBUF_RVT_TT_08302018.v
